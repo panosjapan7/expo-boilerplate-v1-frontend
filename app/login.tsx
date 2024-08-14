9e88ddb2c7013a469660a5b7fb57ece60406d243
panosjapan7 <panosjapan@gmail.com>
2024-08-14 14:07:52 +0200

    3.d Navigate Between Basic Screens with the Stack Navigator

--- app/login.tsx
+++ app/login.tsx
@@ -1,14 +1,10 @@
 // ./app/login.tsx
-import { router } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const Login = () => {
   return (
     <View>
       <Text>Login Screen</Text>
-      <Pressable onPress={() => router.back()}>
-        <Text>Go Back</Text>
-      </Pressable>
     </View>
   );
 };
--- app/register.tsx
+++ app/register.tsx
@@ -1,14 +1,10 @@
 // ./app/register.tsx
-import { router } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const Register = () => {
   return (
     <View>
       <Text>Register Screen</Text>
-      <Pressable onPress={() => router.back()}>
-        <Text>Go Back</Text>
-      </Pressable>
     </View>
   );
 };
