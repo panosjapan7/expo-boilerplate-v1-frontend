02ea2028df97ad76b61b003ac27645bc56ad9f33
panosjapan7 <panosjapan@gmail.com>
2024-08-14 13:56:30 +0200

    3.b Create Basic Screens

---
+++ app/register.tsx
@@ -0,0 +1,12 @@
+// ./app/register.tsx
+import { Text, View } from "react-native";
+
+const Register = () => {
+  return (
+    <View>
+      <Text>Register Screen</Text>
+    </View>
+  );
+};
+
+export default Register;
