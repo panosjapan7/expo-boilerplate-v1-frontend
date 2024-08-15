613d4e14d7489b4f45c72ce407069f5e34997b5d
panosjapan7 <panosjapan@gmail.com>
2024-08-15 13:20:35 +0200

    4.b Create Root-level Web Screens

---
+++ app/register.web.tsx
@@ -0,0 +1,14 @@
+// ./app/register.web.tsx
+import "../styles/css/register.css";
+
+const Register = () => {
+  return (
+    <div className="register-wrapper">
+      <div className="contents-container">
+        <p>Register Screen (web)</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
