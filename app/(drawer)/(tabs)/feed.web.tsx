473e920315612e9f61e9f7110836b8b3a9d0462a
panosjapan7 <panosjapan@gmail.com>
2024-08-16 09:48:02 +0200

    7.b Tab Navigator (web)

---
+++ app/(drawer)/(tabs)/feed.web.tsx
@@ -0,0 +1,14 @@
+// ./app/(drawer)/(tabs)/feed.web.tsx
+import "../../../styles/css/feed.css";
+
+const Feed = () => {
+  return (
+    <div className="feed-wrapper">
+      <div className="contents-container">
+        <p>Feed Screen (web)</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
---
+++ app/(drawer)/(tabs)/profile.web.tsx
@@ -0,0 +1,14 @@
+// ./app/(drawer)/(tabs)/profile.web.tsx
+import "../../../styles/css/profile.css";
+
+const Feed = () => {
+  return (
+    <div className="profile-wrapper">
+      <div className="contents-container">
+        <p>Profile Screen (web)</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
