3f9e50ff7ec756734783c248fde5473019a9c559
panosjapan7 <panosjapan@gmail.com>
2024-08-16 09:39:01 +0200

    7.a.7 Move Tabs Navigator in its own file

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -1,24 +1,8 @@
 // ./app/(drawer)/(tabs)/_layout.tsx
-import { Tabs } from "expo-router";
-import { DrawerToggleButton } from "@react-navigation/drawer";
+import TabsLayoutMobile from "../../../components/layouts/TabsLayoutMobile";
 
 const TabsLayout = () => {
-  return (
-    <Tabs screenOptions={{ headerLeft: () => <DrawerToggleButton /> }}>
-      <Tabs.Screen
-        name="feed"
-        options={{ headerTitle: "Feed", tabBarLabel: "Feed" }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{ headerTitle: "Profile", tabBarLabel: "Profile" }}
-      />
-      <Tabs.Screen
-        name="hidden"
-        options={{ headerTitle: "Hidden", tabBarLabel: "Hidden", href: null }}
-      />
-    </Tabs>
-  );
+  return <TabsLayoutMobile />;
 };
 
 export default TabsLayout;
