a4b3edb3a2bbefb67d166a1b56e54268beeefd29
panosjapan7 <panosjapan@gmail.com>
2024-08-16 09:16:32 +0200

    7.a Tab Navigator (mobile)

---
+++ app/(drawer)/(tabs)/feed.tsx
@@ -0,0 +1,12 @@
+// ./app/(drawer)/(tabs)/feed.tsx
+import { Text, View } from "react-native";
+
+const Feed = () => {
+  return (
+    <View>
+      <Text>Feed Screen</Text>
+    </View>
+  );
+};
+
+export default Feed;
