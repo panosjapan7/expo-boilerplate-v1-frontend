79d59bee74199b94f6da11c896f2d3f9fabad763
panosjapan7 <panosjapan@gmail.com>
2024-08-19 16:13:35 +0200

    12.d Render Forms in Mobile Screens

--- app/login.tsx
+++ app/login.tsx
@@ -1,27 +1,8 @@
 // ./app/login.tsx
-import { Pressable, Text, View } from "react-native";
-import { Link, router } from "expo-router";
-
-import { useGlobalStyles } from "../styles/stylesheets/globalStyles";
+import FormLoginMobile from "../components/forms/FormLoginMobile";
 
 const Login = () => {
-  const { globalStyles } = useGlobalStyles();
-
-  const handleLogin = () => {
-    router.replace("/(drawer)/(tabs)/feed");
-  };
-
-  return (
-    <View style={globalStyles.container}>
-      <Text style={globalStyles.textBlack}>Login Screen</Text>
-      <Pressable onPress={handleLogin}>
-        <Text style={globalStyles.textRegular}>Log in</Text>
-      </Pressable>
-      <Link href="/reset-password">
-        <Text style={globalStyles.textRegular}>Forgot password?</Text>
-      </Link>
-    </View>
-  );
+  return <FormLoginMobile />;
 };
 
 export default Login;
