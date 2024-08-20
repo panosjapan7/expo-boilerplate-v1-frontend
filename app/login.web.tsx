d4476012c21c734ba132581bd7121f728250c57a
panosjapan7 <panosjapan@gmail.com>
2024-08-20 09:05:11 +0200

    13. Registration, Login, Reset Password Forms (Web)

--- app/login.web.tsx
+++ app/login.web.tsx
@@ -1,38 +1,18 @@
-// ./app/login.web.tsx
-import { MouseEvent, useContext } from "react";
-import { router } from "expo-router";
-
-import { AuthContext } from "../contexts/AuthContext";
+// ./client/app/login.web.tsx
+import FormLoginWeb from "../components/forms/FormLoginWeb";
 import "../styles/css/login.css";
 import { useGlobalStyles } from "../styles/stylesheets/globalStyles";
 
 const Login = () => {
-  const { setIsLoggedIn } = useContext(AuthContext);
-  const { themeTextColor } = useGlobalStyles();
-
-  const handleLogin = (e: MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    setIsLoggedIn(true);
-    router.push("/(drawer)/(tabs)/feed");
-  };
+  const { themeBackgroundColor } = useGlobalStyles();
 
   return (
-    <div className="login-wrapper">
+    <div
+      className="login-wrapper"
+      style={{ backgroundColor: themeBackgroundColor }}
+    >
       <div className="contents-container">
-        <p style={{ color: themeTextColor }}>Login Screen (web)</p>
-        <a onClick={handleLogin} style={{ color: themeTextColor }}>
-          Log in
-        </a>
-        <a
-          href="/reset-password"
-          onClick={(e) => {
-            e.preventDefault();
-            router.push("/reset-password");
-          }}
-          style={{ color: themeTextColor }}
-        >
-          Forgot Password?
-        </a>
+        <FormLoginWeb />
       </div>
     </div>
   );
